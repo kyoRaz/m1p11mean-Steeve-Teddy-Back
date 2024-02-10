b6b286de250997af5f0eece7fb590565bec8a405
Kyo <kyostvraz@gmail.com>
2024-02-10 09:45:22 +0300

    refactor  : delai  Service

--- models/Service.js
+++ models/Service.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const serviceSchema = new Schema({
     nom: { type: String, required: true },
-    delai: { type: Number , required: true },
+    delai: { type: String, required: true },
     prix: { type: Number, required: true },
-    commission: { type: Number, required: true  },
+    commission: { type: Number, required: true },
     estActif: { type: Boolean, default: true },
 });
 
