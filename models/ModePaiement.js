1c16d5ddae50c77646261e3c81c01b22d33b2268
Kyo <kyostvraz@gmail.com>
2024-02-15 21:30:17 +0300

    feat : crud mode de paiement

---
+++ models/ModePaiement.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    libelle: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('modePaiement', schema);
