474906f3f747d7565d61bf57c0479e969c4e19a2
Kyo <kyostvraz@gmail.com>
2024-02-15 22:25:47 +0300

    feat : module depense

---
+++ routes/depense.route.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const ctrl = require('../controller/depense.controller');
+
+
+let route = '/depenses';
+
+router.post(`${route}`, ctrl.create);
+
+
+router.get(`${route}`, ctrl.findAll);
+
+
+router.put(`${route}/:id`, ctrl.update);
+
+
+router.delete(`${route}/:id`, ctrl.delete);
+router.get(`${route}/:id`, ctrl.findById);
+
+module.exports = router;
